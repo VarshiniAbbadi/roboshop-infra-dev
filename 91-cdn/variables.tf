2f3469dc1d2051538f21a99f70401b1434b6cb11
varshini <varshini.reddy1@gmail.com>
2025-07-11 21:53:10 +1000

    updated

---
+++ 91-cdn/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z00764991B6VJUY847833"
+}
+
+variable "zone_name" {
+    default = "varshin.xyz"
+}
